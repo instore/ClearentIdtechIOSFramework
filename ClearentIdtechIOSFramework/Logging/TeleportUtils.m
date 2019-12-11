f993f9fe31f9c57989300c96219c8086ac71ca7d
Shaun Reynolds <shaunreynolds@gmail.com>
2019-12-11 12:57:08 -0800

    Removed an unused variable to silence the compiler warning

--- ClearentIdtechIOSFramework/Logging/TeleportUtils.m
+++ ClearentIdtechIOSFramework/Logging/TeleportUtils.m
@@ -14,12 +14,6 @@
 + (void)teleportDebug:(NSString *)format, ... {
 
     if (TELEPORT_DEBUG) {
-#ifdef DEBUG  //in dev mode, write to stdout so that it won't get redirected
-        FILE *out = stdout;
-#else       // otherwise, write to stderr so that I can see how it works in real device
-        FILE *out = stderr;
-#endif
-        
         NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
         [dateFormat setDateFormat:@"HH:mm:ss:SSS"];
         //NSDate *now = [[NSDate alloc] init];
@@ -29,8 +23,6 @@
         va_start(args, format);
         contents = [[NSString alloc] initWithFormat:format arguments:args];
         va_end(args);
-        //fprintf(out, "%s - %s\n", [timeString UTF8String], [contents UTF8String]);
-        
     }
 
 }
